1839b9a947ea0e1ef3c0689f20cf2e2168defcf4
souchardani <114416167+souchardani@users.noreply.github.com>
2024-02-04 15:02:06 +0100

    inicio clase 8

---
+++ relationships/database/migrations/2024_02_04_135455_create_phones_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger("prefix");
+            $table->unsignedBigInteger("phone_number");
+            //generamos clave foranea, por defecto es unsigned big integer
+            //la nomenclatura para la relacion es el nombre del modelo en singular, junto a _id
+            $table->unsignedBigInteger("user_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('phones');
+    }
+};
